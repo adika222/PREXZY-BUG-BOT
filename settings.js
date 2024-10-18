19faf5533ee175a6ee6aff1becd462fcb4d7d200
adika222 <adikadalton.chuks@gmail.com>
2024-10-18 10:55:16 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2347016510719'  //creator number
+global.ownername = 'Adika' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
